cff865d2aa102fd220448b3de1631e1ac7bcc46b
shouta.kawaguti <xiangduochuankou964@gmail.com>
2020-11-08 19:16:39 +0900

    refs#3 タイトル変更、設定画面の追加、マウス感度設定の追加、アイテムのトリガー化

---
+++ Assets/Script/Edit.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Edit : MonoBehaviour
+{
+    /*
+        設定項目を扱うスクリプト
+     */
+
+    private GameController gameController;
+
+    [Space(2)]
+    private GameObject settingUI;
+
+    [Space(2)]
+    //マウス感度割合
+    [SerializeField] private GameObject mouseSensitivilitySliderObg;
+    private Slider mouseSensitivilitySlider;
+
+    [SerializeField] private float minMouseSensibility = 0.01f;
+    [SerializeField] private float maxMouseSensibility = 1.0f;
+    [SerializeField] private float defaultMouseSensibility = 0.5f;
+
+    private void Start() {
+        gameController = GetComponent<GameController>();
+
+        mouseSensitivilitySlider = mouseSensitivilitySliderObg.GetComponent<Slider>();
+        SetMouseSensivility();
+    }
+
+    private void SetMouseSensivility() {
+        mouseSensitivilitySlider.maxValue = maxMouseSensibility;
+        mouseSensitivilitySlider.minValue = minMouseSensibility;
+        mouseSensitivilitySlider.value = defaultMouseSensibility;
+    }
+    public float MouseSensitivilityRatio {
+        get { return mouseSensitivilitySlider.value; }
+    }
+}
